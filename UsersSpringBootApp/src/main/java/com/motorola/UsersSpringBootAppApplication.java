4dfed1de0c903469f6979a4cdcea9b62ebb01a81
Gabrielmzoni <gabrielmzoni@gmail.com>
2022-10-21 09:07:33 -0300

    Project Start

---
+++ UsersSpringBootApp/src/main/java/com/motorola/UsersSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.motorola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersSpringBootAppApplication.class, args);
+	}
+
+}
